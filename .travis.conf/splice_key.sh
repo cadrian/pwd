2e1a1503c989e76465625d52c712ac300357e1b1
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-11 16:05:42 +0100

    cleanup

--- .travis.conf/splice_key.sh
+++ .travis.conf/splice_key.sh
@@ -4,7 +4,9 @@ ID_RSA=$HOME/.ssh/id_rsa_travis
 
 test -d $HOME/.ssh || mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
-rm -f ${ID_RSA}.b64 && touch ${ID_RSA}.b64
-echo -n $__B64_ID_RSA_{00..74} >> ${ID_RSA}.b64
-base64 --decode --ignore-garbage ${ID_RSA}.b64 > $ID_RSA
+{
+    echo -n $__B64_ID_RSA_{00..74}
+    echo
+} | base64 --decode --ignore-garbage > $ID_RSA
 chmod 600 $ID_RSA
+ls -l $ID_RSA
