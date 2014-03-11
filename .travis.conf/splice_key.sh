e02139c97f1c98dd5293b242c84b58f8491fb03f
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-11 15:47:37 +0100

    cosmetics

--- .travis.conf/splice_key.sh
+++ .travis.conf/splice_key.sh
@@ -2,6 +2,9 @@
 
 ID_RSA=$HOME/.ssh/id_rsa_travis
 
-echo -n $__B64_ID_RSA_{00..75} >> ${ID_RSA}.b64
+test -d $HOME/.ssh || mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+rm -f ${ID_RSA}.b64 && touch ${ID_RSA}.b64
+echo -n $__B64_ID_RSA_{00..74} >> ${ID_RSA}.b64
 base64 --decode --ignore-garbage ${ID_RSA}.b64 > $ID_RSA
 chmod 600 $ID_RSA
