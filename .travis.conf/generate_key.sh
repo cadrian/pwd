d5a10f1c43d6350b99fe735c68bd9cf590a5728c
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-11 15:24:39 +0100

    travis access

--- .travis.conf/generate_key.sh
+++ .travis.conf/generate_key.sh
@@ -2,7 +2,9 @@
 
 ID_RSA=$HOME/.ssh/id_rsa_travis
 
+rm -f $ID_RSA
 ssh-keygen -t rsa -C "cyril.adrian@gmail.com" -f $ID_RSA -N ""
 base64 --wrap=0 $ID_RSA > ${ID_RSA}.b64
 ENCRYPTION_FILTER="echo \$(echo \"- secure: \")\$(travis encrypt \"\$FILE='\`cat $FILE\`'\" -r cadrian/pwd)"
-split --bytes=100 --numeric-suffixes --suffix-length=2 --filter="$ENCRYPTION_FILTER" ${ID_RSA}.b64 __B64_ID_RSA_
+split --bytes=30 --numeric-suffixes --suffix-length=2 --filter="$ENCRYPTION_FILTER" ${ID_RSA}.b64 __B64_ID_RSA_ > travis_key.yml
+wc -l travis_key.yml
--- .travis.conf/splice_key.sh
+++ .travis.conf/splice_key.sh
@@ -2,6 +2,6 @@
 
 ID_RSA=$HOME/.ssh/id_rsa_travis
 
-echo -n $__B64_ID_RSA_{00..30} >> ${ID_RSA}.b64
+echo -n $__B64_ID_RSA_{00..75} >> ${ID_RSA}.b64
 base64 --decode --ignore-garbage ${ID_RSA}.b64 > $ID_RSA
 chmod 600 $ID_RSA
