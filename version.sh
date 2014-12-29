efdf0c8e57dff861fed291908231055ecc80b354
Cyril Adrian <cyril.adrian@gmail.com>
2014-12-29 14:41:16 +0100

    better version script

--- version.sh
+++ version.sh
@@ -11,10 +11,10 @@
 dir=$(dirname $(readlink -f $0))
 test -e $dir/.version || {
     echo $(
-        if dpkg -l liberty-eiffel-tools 2>/dev/null; then
-            version=$(dpkg -l liberty-eiffel-tools | tail -n 1 | awk '{print $3}')
+        if dpkg -s liberty-eiffel-tools >/dev/null 2>&1; then
+            version=$(dpkg -s liberty-eiffel-tools | awk '/^Version:/ {print $2; exit}')
         else
-            version=$(se --version | awk 'NR==4 {print $2;exit}')
+            version=$(se --version | awk '$1 == "release" {print $2; exit}')
         fi
         head -n 1 $dir/debian/changelog | awk -F'[()]' '{print $2}' | \
             sed -r "s/#SNAPSHOT#/$(date -u +'~%Y%m%d.%H%M%S')~liberty-eiffel-$version/"
