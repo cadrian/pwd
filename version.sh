09db6bfacba7180b731dfad9fa583246e2f28eea
Cyril Adrian <cyril.adrian@gmail.com>
2014-12-29 13:05:21 +0100

    better version numbering: added Liberty Eiffel's version number

--- version.sh
+++ version.sh
@@ -9,5 +9,15 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 dir=$(dirname $(readlink -f $0))
-test -e $dir/.version || echo $(head -n 1 $dir/debian/changelog | awk -F'[()]' '{print $2}' | sed -r "s/#SNAPSHOT#/$(date -u +'~%Y%m%d%H%M%S')/") > $dir/.version
+test -e $dir/.version || {
+    echo $(
+        if dpkg -l liberty-eiffel-tools 2>/dev/null; then
+            version=$(dpkg -l liberty-eiffel-tools | tail -n 1 | awk '{print $3}')
+        else
+            version=$(se --version | awk 'NR==4 {print $2;exit}')
+        fi
+        head -n 1 $dir/debian/changelog | awk -F'[()]' '{print $2}' | \
+            sed -r "s/#SNAPSHOT#/$(date -u +'~%Y%m%d.%H%M%S')~liberty-eiffel-$version/"
+        ) > $dir/.version
+}
 cat $dir/.version
