487dcdee27b7fb78af987097fd9e0d7afcb0b851
Cyril Adrian <cyril.adrian@gmail.com>
2015-10-22 23:41:51 +0200

    a working clipboard implementation, thanks to https://zenorocha.github.io/clipboard.js/

--- web/static/webclient.js
+++ web/static/webclient.js
@@ -1,13 +1,25 @@
-function copy() {
-    var source = document.getElementById('copytext');
-    var target = document.getElementById('holdtext');
-    target.innerText = source.innerText;
-    var copied = target.createTextRange();
-    copied.execCommand("Copy");
-}
-
 function submit_passform(path) {
     var form = document.getElementById('passform');
     form.action = path;
     form.submit();
+}
+
+function setClipboard() {
+    var clipboard = new Clipboard('.clip');
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+
+        e.clearSelection();
+
+        alert("copied.");
+    });
+
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+        alert("copy failed!");
+    });
 }
